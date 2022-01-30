a4777921dad04fd1a0d4488c9c65394d5f2ebe11
Mary White <mary.a.white28@gmail.com>
2022-01-30 16:01:09 -0600

    added a js file'

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+console.log("Hello, are you looking for a Developer?  Find me on LinkedIn, https://www.linkedin.com/in/marychindwhite/")
+
+console.log(" ▄▄       ▄▄  ▄         ▄ \n" +
+    "▐░░▌     ▐░░▌▐░▌       ▐░▌\n" +
+    "▐░▌░▌   ▐░▐░▌▐░▌       ▐░▌\n" +
+    "▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌\n" +
+    "▐░▌ ▐░▐░▌ ▐░▌▐░▌   ▄   ▐░▌\n" +
+    "▐░▌  ▐░▌  ▐░▌▐░▌  ▐░▌  ▐░▌\n" +
+    "▐░▌   ▀   ▐░▌▐░▌ ▐░▌░▌ ▐░▌\n" +
+    "▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌\n" +
+    "▐░▌       ▐░▌▐░▌░▌   ▐░▐░▌\n" +
+    "▐░▌       ▐░▌▐░░▌     ▐░░▌\n" +
+    " ▀         ▀  ▀▀       ▀▀ ")
